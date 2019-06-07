23b2b3b7337df8e03909b83b0b4a82a24b7126c5
joris <joris@notifica.re>
2019-06-07 15:21:11 +0200

    Updated to Node LTS

--- test/crypto.js
+++ test/crypto.js
@@ -1,17 +1,17 @@
-var Crypto = require('../lib/util/crypto');
+const Crypto = require('../lib/util/crypto');
     expect = require('chai').expect;
 
 describe('HMAC', function() {
 
-    var data = 'some simple test data';
-    var data2 = 'some other simple test data';
-    var key = 'a test key';
-    var expected = 'ddDzXOU6EMrITLgRKBIbhBAHxWAZBUj1nTk9Exs5PNE=';
+    const data = 'some simple test data';
+    const data2 = 'some other simple test data';
+    const key = 'a test key';
+    const expected = 'ddDzXOU6EMrITLgRKBIbhBAHxWAZBUj1nTk9Exs5PNE=';
 
     describe('when generating a HMAC', function() {
 
         it('should generate the expected hmac', function () {
-            var hmac = Crypto.generateHmacBase64String(data, key);
+            const hmac = Crypto.generateHmacBase64String(data, key);
             expect(hmac).to.equal(expected);
         });
     });
@@ -19,16 +19,16 @@ describe('HMAC', function() {
     describe('when comparing HMAC Strings', function() {
 
         it('should return true for equal HMAC', function () {
-            var hmac1 = Crypto.generateHmacBase64String(data, key);
-            var hmac2 = Crypto.generateHmacBase64String(data, key);
-            var compare = Crypto.compareHmac(hmac1, hmac2);
+            const hmac1 = Crypto.generateHmacBase64String(data, key);
+            const hmac2 = Crypto.generateHmacBase64String(data, key);
+            const compare = Crypto.compareHmac(hmac1, hmac2);
             expect(compare).to.be.true;
         });
 
         it('should return false for non-equal HMAC', function () {
-            var hmac1 = Crypto.generateHmacBase64String(data, key);
-            var hmac2 = Crypto.generateHmacBase64String(data, key);
-            var compare = Crypto.compareHmac(hmac1, hmac2);
+            const hmac1 = Crypto.generateHmacBase64String(data, key);
+            const hmac2 = Crypto.generateHmacBase64String(data, key);
+            const compare = Crypto.compareHmac(hmac1, hmac2);
             expect(compare).to.be.true;
         });
     });
@@ -36,16 +36,16 @@ describe('HMAC', function() {
     describe('when comparing HMAC Buffers', function() {
 
         it('should return true for equal HMAC', function() {
-            var hmac1 = Crypto.generateHmac(data, key);
-            var hmac2 = Crypto.generateHmac(data, key);
-            var compare = Crypto.compareHmac(hmac1, hmac2);
+            const hmac1 = Crypto.generateHmac(data, key);
+            const hmac2 = Crypto.generateHmac(data, key);
+            const compare = Crypto.compareHmac(hmac1, hmac2);
             expect(compare).to.be.true;
         });
 
         it('should return false for non-equal HMAC', function() {
-            var hmac1 = Crypto.generateHmac(data, key);
-            var hmac2 = Crypto.generateHmac(data2, key);
-            var compare = Crypto.compareHmac(hmac1, hmac2);
+            const hmac1 = Crypto.generateHmac(data, key);
+            const hmac2 = Crypto.generateHmac(data2, key);
+            const compare = Crypto.compareHmac(hmac1, hmac2);
             expect(compare).to.not.be.true;
         });
 
@@ -54,16 +54,16 @@ describe('HMAC', function() {
     describe('when comparing HMAC Buffers with HMAC Strings', function() {
 
         it('should return true for equal HMAC', function() {
-            var hmac1 = Crypto.generateHmacBase64String(data, key);
-            var hmac2 = Crypto.generateHmac(data, key);
-            var compare = Crypto.compareHmac(hmac1, hmac2);
+            const hmac1 = Crypto.generateHmacBase64String(data, key);
+            const hmac2 = Crypto.generateHmac(data, key);
+            const compare = Crypto.compareHmac(hmac1, hmac2);
             expect(compare).to.be.true;
         });
 
         it('should return false for non-equal HMAC', function() {
-            var hmac1 = Crypto.generateHmacBase64String(data, key);
-            var hmac2 = Crypto.generateHmac(data2, key);
-            var compare = Crypto.compareHmac(hmac1, hmac2);
+            const hmac1 = Crypto.generateHmacBase64String(data, key);
+            const hmac2 = Crypto.generateHmac(data2, key);
+            const compare = Crypto.compareHmac(hmac1, hmac2);
             expect(compare).to.not.be.true;
         });
 
--- test/http-gateway.js
+++ test/http-gateway.js
@@ -1,9 +1,9 @@
-var LiveApi = require('../lib/live-api');
+const LiveApi = require('../lib/live-api');
     expect = require('chai').expect;
 
 describe('HttpGateway', function() {
 
-    var data = 'some simple test data',
+    const data = 'some simple test data',
         data2 = 'some other simple test data',
         privateKey = 'a test key',
         publicKey = 'another test key',
@@ -15,13 +15,13 @@ describe('HttpGateway', function() {
     describe('when verifying a Live API call', function() {
 
         it('should return the expected hmac', function () {
-            var hmac = httpGateway.verify(publicKey, data);
+            const hmac = httpGateway.verify(publicKey, data);
             expect(hmac).to.equal(expected);
         });
 
         it('should throw an error for non-matching public key', function () {
-            var test = function() {
-                var hmac = httpGateway.verify('xxxxxxx', data);
+            const test = function() {
+                const hmac = httpGateway.verify('xxxxxxx', data);
             }
             expect(test).to.throw(Error);
         });
@@ -31,12 +31,12 @@ describe('HttpGateway', function() {
     describe('when validating a Live API call', function() {
 
         it('should return true for matching signature', function () {
-            var response = httpGateway.validate(publicKey, data, expected);
+            const response = httpGateway.validate(publicKey, data, expected);
             expect(response).to.be.true;
         });
 
         it('should return false for non-matching signature', function () {
-            var response = httpGateway.validate(publicKey, data, 'AAAA');
+            const response = httpGateway.validate(publicKey, data, 'AAAA');
             expect(response).to.not.be.true;
         });
 
@@ -46,8 +46,8 @@ describe('HttpGateway', function() {
         });
 
         it('should throw an error for non-matching public key', function () {
-            var test = function() {
-                var response = httpGateway.validate('xxxxxxx', data, expected);
+            const test = function() {
+                const response = httpGateway.validate('xxxxxxx', data, expected);
             }
             expect(test).to.throw(Error);
         });
